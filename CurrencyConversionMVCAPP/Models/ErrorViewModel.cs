99c9d6f3890fccc27439e3b52487a187c48cc102
Admin <Admin@DESKTOP-C8SNRA1>
2021-10-12 15:42:27 +0530

    Add project files.

---
+++ CurrencyConversionMVCAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CurrencyConversionMVCAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
